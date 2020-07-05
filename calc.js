1cf249ff60dd5d800eb48f15ff75a1fc26522c56
Sergey Gorovov <gor-serg@bk.ru>
2020-07-05 22:16:09 +0300

    v0.1

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
+const input = document.querySelector("#Input");
+const result = document.querySelector(".result_num");
+const action = document.querySelector(".result_action");
+
 document.addEventListener('keydown', function(event) {
     if (event.code === 'Backspace' ||
         event.code === 'KeyZ' && (event.ctrlKey || event.metaKey)) {
@@ -6,11 +10,11 @@ document.addEventListener('keydown', function(event) {
     }
     let key = Number(event.key)
     if (!isNaN(key) && event.key !== null && event.key !== ' ') {
-        input(event.key);
+        input_number(event.key);
         return;
     }
     if (event.key === "." || event.key ===",") {
-        input(".");
+        input_number(".");
         return;
     }
     if (event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
@@ -18,52 +22,87 @@ document.addEventListener('keydown', function(event) {
         return;
     }
     if (event.key === "Enter") {
-        calculate();
+        input_action("=");
     }
 
 });
 
-function input(n) {
-    if (document.querySelector("#Eval").textContent.length < 16) {
-        document.querySelector("#Eval").textContent += n;
+function input_number(n) {
+    if (input.textContent === "0" && n !== ".") {
+        input.textContent = "";
+    }
+    if (n === ".") {
+        for (let i of input.textContent) if (i === ".") return;
+    }
+    if (input.textContent.length < 16) {
+        input.textContent += n;
     }
 }
 
 function input_action(a) {
-    if (document.querySelector("#Eval").textContent === "" &&
-        document.querySelector("#Result").textContent !== "" &&
-        document.querySelector("#Result").textContent !== "Infinity" &&
-        document.querySelector("#Result").textContent !== "NaN") {
-        document.querySelector("#Eval").textContent = document.querySelector("#Result").textContent
-    }
-    _clearAction();
-    if (document.querySelector("#Eval").textContent.length < 16) {
-        document.querySelector("#Eval").textContent += a;
+    if (result.textContent === "") {
+        result.textContent = input.textContent;
+        action.textContent = (a !== "=") ? a: "";
+        input.textContent = "";
+    } else {
+        if (input.textContent !== "0") {
+            if (action.textContent !== "") {
+                result.textContent = String(Number(calculate().slice(0, 16)));
+                action.textContent = (a !== "=") ? a: "";
+                input.textContent = "0";
+            } else {
+                result.textContent = input.textContent;
+                input.textContent = "0";
+                action.textContent = (a !== "=") ? a: "";
+            }
+        } else {
+            if (a === "=") {
+                result.textContent = String(Number(calculate().slice(0, 16)));
+                action.textContent = "";
+            } else {
+                action.textContent = (a !== "=") ? a: "";
+            }
+        }
     }
 }
 
 function calculate() {
-    _clearAction();
-    document.querySelector("#Result").textContent = eval(document.querySelector("#Eval").textContent);
-    document.querySelector("#Eval").textContent = "";
+    if (action.textContent === "+") {
+        return  String(Number(result.textContent) + Number(input.textContent));
+    }
+    if (action.textContent === "-") {
+        return  String(Number(result.textContent) - Number(input.textContent));
+    }
+    if (action.textContent === "*") {
+        return  String(Number(result.textContent) * Number(input.textContent));
+    }
+    if (action.textContent === "/") {
+        console.log(result.textContent, "/", input.textContent);
+        if (input.textContent === "0") {
+            if (result.textContent === "0") {
+                return "Undefined";
+            }
+            return "0 division error";
+        }
+        return  String(Number(result.textContent) / Number(input.textContent));
+    }
 }
 
 function clear_all() {
-    if (document.querySelector("#Eval").textContent === "") {
-        document.querySelector("#Result").textContent = "";
+    if (input.textContent === "0") {
+        if (action.textContent === "") {
+            result.textContent = "";
+        } else {
+            action.textContent = "";
+        }
     } else {
-        document.querySelector("#Eval").textContent = "";
+        input.textContent = "0";
     }
 }
 
 function backspace() {
-    document.querySelector("#Eval").textContent = document.querySelector("#Eval").textContent.slice(0,-1);
-}
-
-function _clearAction() {
-    let last = document.querySelector("#Eval").textContent.slice(-1);
-    console.log(last);
-    if (last === "+" || last === "-" || last === "*" || last === "/") {
-        backspace();
+    input.textContent = input.textContent.slice(0,-1);
+    if (input.textContent === "") {
+        input.textContent = "0";
     }
 }
